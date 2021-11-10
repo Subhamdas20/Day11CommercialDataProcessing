cea6e6b844af0583baced1ac66312c8bc4595072
Subhamdas20 <subham21.sd@gmail.com>
2021-11-10 18:15:43 +0530

    Added : stock account management

--- src/main/java/com/bridgelabz/stockaccount/StockAccount.java
+++ src/main/java/com/bridgelabz/stockaccount/StockAccount.java
@@ -1,12 +1,10 @@
 package com.bridgelabz.stockaccount;
 
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -22,8 +20,33 @@ public class StockAccount {
     public StockAccount() throws IOException, ParseException {
     }
 
-    public void buy() {
-
+    /**
+     * method to initiate buy
+     *
+     * @param symbol is used to get the symbol of stock to buy
+     */
+    public void buy(String symbol, String name_Of_Stock) throws IOException {
+        System.out.println("Enter the Quantity to buy");
+        int number_Of_Stocks = sc.nextInt();
+        System.out.println("Enter the price");
+        int price = sc.nextInt();
+        int total_Buy_Value = number_Of_Stocks * price;
+        Long account_Balance = (Long) accounts.get("balance");
+        int account_Balance_After_Buy = (int) (account_Balance - total_Buy_Value);
+        if (total_Buy_Value <= account_Balance && total_Buy_Value >= 0) {
+            JSONArray stock = (JSONArray) accounts.get("stock");
+            accounts.put("balance", account_Balance_After_Buy);
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("symbol", symbol);
+            jsonObject.put("name", name_Of_Stock);
+            jsonObject.put("quantity", number_Of_Stocks);
+            jsonObject.put("price", price);
+            jsonObject.put("value", total_Buy_Value);
+            stock.add(jsonObject);
+            FileWriter file = new FileWriter("E:\\Day11commercialdataprocessing\\src\\main\\java\\com\\bridgelabz\\jsonfiles\\account.json");
+            file.write(accounts.toJSONString());
+            file.close();
+        }
     }
 
     /**
@@ -31,7 +54,7 @@ public class StockAccount {
      *
      * @param symbol to check if the specified stock with symbol is present
      */
-    public Long searchStock(String symbol) {
+    private Long searchStock(String symbol) {
         JSONArray stockDetails = (JSONArray) accounts.get("stock");
         for (int i = 0; i < stockDetails.size(); i++) {
             JSONObject stockSymbolDetails = (JSONObject) stockDetails.get(i);
@@ -44,10 +67,12 @@ public class StockAccount {
         return 0L;
     }
 
-    /*
+    /**
      * method to initiate sell process
-     * */
-    public void sell(String stockSymbol) throws IOException {
+     *
+     * @param stockSymbol is used to get the stock symbol to sell .
+     */
+    private void sell(String stockSymbol) throws IOException {
         Long quantity = searchStock(stockSymbol);
         System.out.println("Enter the quantity to sell");
         int sell_Quantity = sc.nextInt();
@@ -82,11 +107,11 @@ public class StockAccount {
     /*
      * method to print stock details
      * */
-    public void stockReport() {
+    private void stockReport() {
         JSONArray stockDetails = (JSONArray) accounts.get("stock");                       //Array of stocks
         for (int i = 0; i < stockDetails.size(); i++) {
             JSONObject stockSymbolDetails = (JSONObject) stockDetails.get(i);
-            System.out.println("Details of " + stockSymbolDetails.get("name")+" is");
+            System.out.println("Details of " + stockSymbolDetails.get("name") + " is");
             System.out.println("Stock Symbol is : " + stockSymbolDetails.get("symbol"));
             System.out.println("Stock quantity is : " + stockSymbolDetails.get("quantity"));
             System.out.println("Per Stock price is : " + stockSymbolDetails.get("price"));
@@ -95,19 +120,51 @@ public class StockAccount {
         }
     }
 
+    /**
+     * debit method is used to withdraw
+     *
+     * @param withdraw_amount is used to get the withdraw amount
+     */
+    private void debit(int withdraw_amount) throws IOException {
+        Long account_Balance = (Long) accounts.get("balance");
+        if (withdraw_amount > 0 && withdraw_amount <= account_Balance) {
+            int remaining_Balance = (int) (account_Balance - withdraw_amount);
+            accounts.put("balance", remaining_Balance);
+            System.out.println("Withdraw successful");
+        } else System.out.println("Debit amount exceeds account balance");
+        FileWriter file = new FileWriter("E:\\Day11commercialdataprocessing\\src\\main\\java\\com\\bridgelabz\\jsonfiles\\account.json");
+        file.write(accounts.toJSONString());
+        file.close();
+    }
+
     public static void main(String[] args) throws IOException, ParseException {
         StockAccount account = new StockAccount();
-        System.out.println("Enter 1 to sell 2 for stock report");
-        switch (sc.nextInt()){
-            case 1 :
+        System.out.println("Enter 1 to buy, 2 to sell, 3 to debit, 4 for stock report");
+        switch (sc.nextInt()) {
+            case 1:
+                System.out.println("Enter the stock symbol to buy");
+                String stock_To_Buy = sc.next();
+                System.out.println("Enter the name of stock");
+                String name_Of_Stock = sc.next();
+                account.buy(stock_To_Buy, name_Of_Stock);
+                break;
+            case 2:
                 System.out.println("Enter the stock symbol to sell");
                 String stock_To_Sell = sc.next();
                 account.sell(stock_To_Sell);
-            break;
-            case 2 :
+                break;
+            case 3:
+                System.out.println("Enter the amount to withdraw");
+                int withdraw_Amount = sc.nextInt();
+                account.debit(withdraw_Amount);
+                break;
+            case 4:
                 account.stockReport();
-            break;
+                break;
             default:
                 System.out.println("Invalid input");
-    }}
+        }
+    }
+
+
 }
